895cc009307907cbe4a9ba82182578c867f4dfd5
Nicholas Gulachek <nsgulachek@gmail.com>
2025-08-17 15:49:20 -0500

    Add correspondence between e1 and plan.yaml. Currently only in documentation

--- src/spec/pkg/a/src/e1.c
+++ src/spec/pkg/a/src/e1.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
 int main() {
+	/*
+	 * Assuming this  will be run from vendor/bin after installing, this
+	 * proves that the exe is installed correctly
+	 */
+	printf("e2e.addExecutable.install-to-bin = pass\n");
 	return 0;
 }
